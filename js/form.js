80169b246478852c25880c08e92f1803749ab12f
adebowaleay <54648067+adebowaleay@users.noreply.github.com>
2020-02-25 13:36:36 -0600

    Update form.js
    changing variables checked for

--- js/form.js
+++ js/form.js
@@ -1,9 +1,9 @@
 function validatetextForm() {
 var a = document.forms["apptsignup"]["enterprisename"].value;
 var b = document.forms["apptsignup"]["contactname"].value;
-var c = document.forms["apptsignup"]["enterprisename"].value;
-var d = document.forms["apptsignup"]["contactname"].value;
-var e = document.forms["apptsignup"]["enterprisename"].value;
+var c = document.forms["apptsignup"]["email"].value;
+var d = document.forms["apptsignup"]["contactnumber"].value;
+var e = document.forms["apptsignup"]["dateofbirth"].value;
 if (a == "" || b == "" || c == "" || d == "" || e == "") {
 alert("Fields cannot be empty.");
 return false;}
